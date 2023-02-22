68009d3122c2c969e7f76a2bd1fd362fbc622dc8
DHIVAKAR <dave.barea@gmail.com>
2023-02-22 10:25:52 +0530

    New Changes

---
+++ Data_Structure/Tree/BinarySearch Tree.py
@@ -0,0 +1,10 @@
+#internal implementation of set is BST
+
+# insertion and access elemrn in 0(n)
+#BFS
+#DFS --> Inorder -> (left, root, right) , preorder --> root, left, right, post order--> left, right, root
+
+class BST:
+    def __init__(self):
+        self.data = data
+        self
